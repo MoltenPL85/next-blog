5768570ff2ac982b0e1ac889c68049a1f92e41ca
MoltenPL85 <bugaev.maxim.85@gmail.com>
2021-03-31 20:28:54 +0500

    Overriding default img tag in markdown

--- conponents/posts/post-detail/post-content.tsx
+++ conponents/posts/post-detail/post-content.tsx
@@ -1,7 +1,9 @@
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import { Post } from '../../../interfaces';
+import { ElementType } from 'react';
 
 interface PostContentProps {
   post: Post;
@@ -10,10 +12,43 @@ interface PostContentProps {
 const PostContent = ({ post }: PostContentProps) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].type === 'image') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
